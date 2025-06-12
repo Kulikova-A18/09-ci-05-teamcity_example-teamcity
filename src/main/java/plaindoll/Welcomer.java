d83ec062bdf4d5f021fa5114711ccd7e2bf3ae70
Kulikova-A18 <81427431+Kulikova-A18@users.noreply.github.com>
2025-06-12 11:06:51 +0300

    новый метод для класса Welcomer

--- src/main/java/plaindoll/Welcomer.java
+++ src/main/java/plaindoll/Welcomer.java
@@ -13,4 +13,7 @@ public class Welcomer{
 	public String saySome(){
 		return "something in the way";
 	}
+	public String sayWelcomeNew(){
+		return "Welcome NEW home. What is it your desire!!!";
+	}
 }
